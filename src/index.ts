6bc703765eb68a1c2e0817f1be13c4b41193f480
Shinya Fujino <shf0811@gmail.com>
2022-09-24 00:49:29 +0900

    With KV

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
 import { Hono } from 'hono'
 import { customAlphabet } from 'nanoid'
 
-const app = new Hono()
+interface Env {
+  SHORT_URLS: KVNamespace
+}
 
-const store: {
-  [key: string]: { url: string; createdAt: string } | undefined
-} = {}
+interface URL {
+  url: string
+  createdAt: number
+}
+
+const app = new Hono<{ Bindings: Env }>()
 
 const nanoid = customAlphabet(
   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
   7,
 )
 
-function shorten(url: string) {
+async function shorten(kv: KVNamespace, url: string) {
   const key = nanoid()
-  const createdAt = new Date().toISOString()
-  store[key] = { url, createdAt }
+  const createdAt = Date.now()
+  await kv.put(key, JSON.stringify({ url, createdAt }))
   return { key }
 }
 
-function getUrl(key: string) {
-  return store[key]
+async function getUrl(kv: KVNamespace, key: string) {
+  return kv.get<URL>(key, 'json')
 }
 
 app.get('/', (c) => c.text('shtn'))
@@ -30,13 +35,13 @@ app.post('/api/links', async (c) => {
   if (!url) {
     return c.text('Missing url', 400)
   }
-  const { key } = shorten(url)
+  const { key } = await shorten(c.env.SHORT_URLS, url)
   return c.json({ key, url })
 })
 
-app.get('/api/links/:key', (c) => {
+app.get('/api/links/:key', async (c) => {
   const key = c.req.param('key')
-  const res = getUrl(key)
+  const res = await getUrl(c.env.SHORT_URLS, key)
   if (!res) {
     return c.notFound()
   }
