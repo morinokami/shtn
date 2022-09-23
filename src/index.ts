63a0325e1ffa7ce6e503d21369e6fd98889a5a4d
Shinya Fujino <shf0811@gmail.com>
2022-09-23 19:15:23 +0900

    Without KV

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,46 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono'
+import { customAlphabet } from 'nanoid'
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+const app = new Hono()
+
+const store: {
+  [key: string]: { url: string; createdAt: string } | undefined
+} = {}
+
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  7,
+)
+
+function shorten(url: string) {
+  const key = nanoid()
+  const createdAt = new Date().toISOString()
+  store[key] = { url, createdAt }
+  return { key }
 }
 
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
+function getUrl(key: string) {
+  return store[key]
+}
+
+app.get('/', (c) => c.text('shtn'))
+
+app.post('/api/links', async (c) => {
+  const { url } = await c.req.json<{ url: string }>()
+  if (!url) {
+    return c.text('Missing url', 400)
+  }
+  const { key } = shorten(url)
+  return c.json({ key, url })
+})
+
+app.get('/api/links/:key', (c) => {
+  const key = c.req.param('key')
+  const res = getUrl(key)
+  if (!res) {
+    return c.notFound()
+  }
+  return c.json({ key, url: res.url })
+})
+
+export default app
